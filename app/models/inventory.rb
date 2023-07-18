bb1cf727d1f6589283b3c2bc39339d676d0a3cdd
Ritika Rawat <Ritikarawat220@gmail.com>
2023-07-18 13:58:32 +0530

    Update details page & models

--- app/models/inventory.rb
+++ app/models/inventory.rb
@@ -2,4 +2,8 @@ class Inventory < ApplicationRecord
   belongs_to :users
   has_many :inventory_foods, dependent: :destroy
   has_many :foods, through: :inventory_foods
+
+  validates :name, presence: true
+  validates :user_id, presence: true
+  validates :description, presence: true
 end
--- app/models/inventory_food.rb
+++ app/models/inventory_food.rb
@@ -1,4 +1,9 @@
 class InventoryFood < ApplicationRecord
   belongs_to :food
   belongs_to :inventory
+
+  validates :quantity, presence: true
+  validates :food_id, presence: true
+  validates :inventory_id, presence: true
+  validates :quantity_unit, presence: true
 end
