eb654be914d0d6389276633256e8fa87c30bba7c
cherelemma <chere.lemma@aastu.edu.et>
2023-07-20 07:12:29 +0300

    remove unnessary comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  
   resources :inventories do
     resources :inventory_foods, only: [:new, :create, :destroy]
   end
