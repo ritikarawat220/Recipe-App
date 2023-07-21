2fcf1aef6af666211290bac52a0b657f0523bd39
cherelemma <chere.lemma@aastu.edu.et>
2023-07-21 15:56:39 +0300

    re-create recipe-foods model

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,5 +1,5 @@
 class RecipeFood < ApplicationRecord
-  belongs_to :recipe, foreign_key: 'recipe_id'
+  belongs_to :recipe, foreign_key: 'recipe_id' 
   belongs_to :food, foreign_key: 'food_id'
 
   validates :quantity, presence: true, numericality: { greater_than: 0 }
