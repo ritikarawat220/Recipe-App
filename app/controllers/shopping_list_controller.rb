554bf55097a7d6affa3b3e1d30ba4d059863dee5
cherelemma <chere.lemma@aastu.edu.et>
2023-07-21 15:58:45 +0300

    add controller for shoping lists

---
+++ app/controllers/shopping_list_controller.rb
@@ -0,0 +1,12 @@
+class ShoppingListController < ApplicationController
+  def index
+    @recipes = Recipe.all
+  end
+  def show
+    @recipe = Recipe.includes(:recipe_foods).find(params[:id])
+    @recipe_id = @recipe.id
+    @inventories = Inventory.all
+
+    render :show
+  end
+end
