83047dcffbc46d85f167881960749fca6d90d415
cherelemma <chere.lemma@aastu.edu.et>
2023-07-22 10:22:59 +0300

    remomve 'status' column and update the schema

--- db/schema.rb
+++ db/schema.rb
@@ -61,7 +61,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_21_122048) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
